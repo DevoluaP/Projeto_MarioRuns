fcef55d6716f7db5ccd054768b49c1872ba7a840
roluaP <paulohenrique456moreira@gmail.com>
2023-05-28 14:37:53 -0300

    feat

--- JS/script.js
+++ JS/script.js
@@ -1,21 +1,31 @@
 const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
+const msg = document.querySelector('#returnGame');
 
+
+
+// jump animation
 const jump = () => {
     mario.classList.add('jump');
 
     setTimeout(() => {
-
         mario.classList.remove('jump');
+    }, 500)
+};
+document.addEventListener('keydown', function(event) {
+    if (event.key === ' ' || event.key === 'ArrowUp' || event.key === 'w') {
+        jump();
+    }
+})
 
-    }, 500);
 
-}
 
+// dead animation
 const loop = setInterval(() => {
 
     const pipePosition = pipe.offsetLeft;
     const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+    const restart = document;
 
     if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
 
@@ -27,12 +37,30 @@ const loop = setInterval(() => {
 
         mario.src = './imagens/game over.png';
         mario.style.width = '120px';
-        mario.style.marginLeft = '8px';
+        mario.style.marginLeft = '10px';
+        msg.style.display = 'flex';
 
         clearInterval(loop);
 
-    }
+        restart.addEventListener('keydown', function(event) {
+            if (event.key) {
+              location.reload()
+            }
+        })
 
+    }
 }, 10);
 
-document.addEventListener('keydown', jump);
+
+
+// score
+document.addEventListener('DOMContentLoaded', function() {
+  let tempo = 0;
+  
+  let temporizador = setInterval( () => {
+    tempo = tempo + 1000;
+    let score = Math.floor((tempo / 100 / 10));
+    document.getElementById('score').innerHTML = score;
+  }, 500)
+
+});
