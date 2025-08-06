1a5117d038d9dceed212b38e83e8b6eeb2a5015d
DevoluaP <devoluap@gmail.com>
2025-08-06 19:59:08 -0300

    fazendo responsividade

--- JS/script.js
+++ JS/script.js
@@ -1,65 +1,52 @@
-// variables
 const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
 const msg = document.querySelector('#returnGame');
 const restart = document;
 
-
-
-// jump animation
 const jump = () => {
-    mario.classList.add('jump');
-
-    setTimeout(() => {
-        mario.classList.remove('jump');
-    }, 500)
+    if (!mario.classList.contains('jump')) {
+        mario.classList.add('jump');
+        setTimeout(() => mario.classList.remove('jump'), 500);
+    }
 };
-document.addEventListener('keydown', function(event) {
+
+document.addEventListener('keydown', (event) => {
     if (event.key === ' ' || event.key === 'ArrowUp' || event.key === 'w') {
         jump();
     }
-})
+});
 
+document.addEventListener('touchstart', () => {
+    jump();
+});
 
-
-// score
-let tempo = 0;  
-let temporizador = setInterval( () => {
-    tempo = tempo + 1000;
+let tempo = 0;
+let temporizador = setInterval(() => {
+    tempo += 1000;
     let score = Math.floor((tempo / 100 / 10));
     document.getElementById('score').innerHTML = score;
-}, 300)    
+}, 300);
 
-
-
-// game over
 const loop = setInterval(() => {
-
     const pipePosition = pipe.offsetLeft;
     const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+    const marioWidth = mario.offsetWidth;
+    const marioHeight = mario.offsetHeight;
 
-    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+    if (pipePosition <= marioWidth && pipePosition > 0 && marioPosition < (marioHeight - 20)) {
         pipe.style.animation = 'none';
         pipe.style.left = `${pipePosition}px`;
 
         mario.style.animation = 'none';
         mario.style.bottom = `${marioPosition}px`;
         mario.src = './imagens/game over.png';
-        mario.style.width = '120px';
+        mario.style.width = `${marioWidth * 0.8}px`;
         mario.style.marginLeft = '10px';
-        
         msg.style.display = 'flex';
 
-        // pausing score and animations
         clearInterval(temporizador);
         clearInterval(loop);
-        
-        // event to restart game
-        restart.addEventListener('keydown', function(event) {
-            if (event.key) {
-              location.reload()
-            }
-        })
+        restart.addEventListener('keydown', () => location.reload());
+        restart.addEventListener('touchstart', () => location.reload());
     }
-
 }, 10);
